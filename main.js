7d971c7234e66da58d0abc5a6c7b27cf97f9cbfb
Tetiana Zetova <tanyazetova@gmail.com>
2023-03-30 16:56:29 +0300

    fixed errors

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
 let n;
+const array = [];
+
 do {
   n = +prompt('Enter the array length');
 } while (isNaN(n) || n < 0);
 console.log(n, 'Array length');
 
-const array = [];
-for (i = 0; i < n; i++) {
-  array.push(Math.round(Math.random() * 100));
+for (let i = 0; i < n; i++) {
+  let number;
+
+  do {
+    number = +prompt('Enter positive number of array for index ' + i);
+  } while (isNaN(number) || number < 0);
+
+  array.push(number);
 }
-console.log(array, 'Array');
+
+console.log(array, 'Entered array');
