708ad0f06f6393e0d8d0704d2839eff2b15cd289
Tetiana Zetova <tanyazetova@gmail.com>
2023-04-11 11:35:00 +0300

    add homework-18

--- main.js
+++ main.js
@@ -1,48 +1,40 @@
-console.log('Задача - 1');
-
-function filter(array, cb) {
-  const result = [];
-
-  for (let index = 0; index < array.length; index++) {
-    if (cb(array[index])) {
-      result.push(array[index]);
-    }
+console.log('Задача-1');
+function reverseRow(n) {
+  if (n === 0) {
+    return;
   }
+  console.log(n);
+  return reverseRow(n - 1);
+}
+reverseRow(5);
+
+console.log('Задача-2');
+function sumTo(array) {
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return sum;
+}
+
+let result = sumTo([1, 2, 3]);
+
+console.log(result, 'sum of array');
+
+console.log('Задача-3');
+
+function calculeteFactorial(n) {
+  if (n === 0 || n === 1) {
+    return 1;
+  }
+
+  const result = n * calculeteFactorial(n - 1);
 
   return result;
 }
 
-const input = [1, 2, 3, 4, 5, 6];
-function isEven(x) {
-  return x % 2 == 0;
-}
-function greater3(x) {
-  return x > 3;
-}
-console.log(input, 'initial array');
-console.log(filter(input, isEven), 'filtered array');
-console.log(filter(input, greater3), 'filtered array');
+let result1 = calculeteFactorial(5);
 
-console.log('Задача - 2');
-
-function copy(array, cb) {
-  const result = [];
-
-  for (let index = 0; index < array.length; index++) {
-    result.push(typeof cb === 'function' ? cb(array[index]) : array[index]);
-  }
-
-  return result;
-}
-
-const list = [3, 5, 6, 7, 9, 1, 2];
-
-console.log(list, 'Initial array');
-console.log(
-  copy(list, (value) => value * 10),
-  'Copied array'
-);
-console.log(
-  copy(list, (value) => value ** 2),
-  'Copied array'
-);
+console.log(result1, 'Factorial');
