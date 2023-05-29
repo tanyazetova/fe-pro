93f05f482da1c8359b66689312ea3661b10a6243
Tetiana Zetova <tanyazetova@gmail.com>
2023-05-29 23:15:38 +0300

    fix

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
-function SuperMath() {
-  this.operations = {
-    '-': (x, y) => x - y,
-    '+': (x, y) => +x + +y,
-    '/': (x, y) => x / y,
-    '*': (x, y) => x * y,
-    '%': (x, y) => (x / y) * 100,
-  };
-}
-SuperMath.prototype.check = function (obj) {
+function SuperMath() {}
+
+SuperMath.prototype.operations = {
+  '-': (x, y) => x - y,
+  '+': (x, y) => +x + +y,
+  '/': (x, y) => x / y,
+  '*': (x, y) => x * y,
+  '%': (x, y) => (x / y) * 100,
+};
+
+const math = new SuperMath();
+
+math.input = function () {
+  const X = prompt('X = ');
+  const Y = prompt('Y = ');
+  const znak = prompt('znak = ');
+  if (!Object.keys(this.operations).includes(znak)) {
+    alert(
+      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
+        ' '
+      )}`
+    );
+    return this.input();
+  }
+  return { X, Y, znak };
+};
+math.check = function (obj) {
   if (
     obj &&
     obj.Y &&
@@ -24,21 +41,5 @@ SuperMath.prototype.check = function (obj) {
     return this.check(this.input());
   }
 };
-SuperMath.prototype.input = function () {
-  const X = prompt('X = ');
-  const Y = prompt('Y = ');
-  const znak = prompt('znak = ');
-  if (!Object.keys(this.operations).includes(znak)) {
-    alert(
-      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
-        ' '
-      )}`
-    );
-    return this.input();
-  }
-  return { X, Y, znak };
-};
 
-const math = new SuperMath();
-console.log(math.check());
 console.log(math.check({ X: 5, Y: 3, znak: '-' }));
