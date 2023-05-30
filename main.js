d0bf55db7fd251ed9881bd8199c6a99775dd80e9
Tetiana Zetova <tanyazetova@gmail.com>
2023-05-30 14:51:19 +0300

    fixed

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
-function SuperMath() {}
+function SuperMath() {
+  this.input = function () {
+    const X = prompt('X = ');
+    const Y = prompt('Y = ');
+    const znak = prompt('znak = ');
+    if (!Object.keys(this.operations).includes(znak)) {
+      alert(
+        `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
+          ' '
+        )}`
+      );
+      return this.input();
+    }
+    return { X, Y, znak };
+  };
+  this.check = function (obj) {
+    if (
+      obj &&
+      obj.Y &&
+      obj.X &&
+      obj.znak &&
+      Object.keys(this.operations).includes(obj.znak)
+    ) {
+      if (confirm(`Do you want to calculate ${obj.X} ${obj.znak} ${obj.Y}`)) {
+        return this.operations[obj.znak](obj.X, obj.Y);
+      } else {
+        return this.check(this.input());
+      }
+    } else {
+      return this.check(this.input());
+    }
+  };
+}
 
 SuperMath.prototype.operations = {
   '-': (x, y) => x - y,
@@ -9,37 +41,5 @@ SuperMath.prototype.operations = {
 };
 
 const math = new SuperMath();
-
-math.input = function () {
-  const X = prompt('X = ');
-  const Y = prompt('Y = ');
-  const znak = prompt('znak = ');
-  if (!Object.keys(this.operations).includes(znak)) {
-    alert(
-      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
-        ' '
-      )}`
-    );
-    return this.input();
-  }
-  return { X, Y, znak };
-};
-math.check = function (obj) {
-  if (
-    obj &&
-    obj.Y &&
-    obj.X &&
-    obj.znak &&
-    Object.keys(this.operations).includes(obj.znak)
-  ) {
-    if (confirm(`Do you want to calculate ${obj.X} ${obj.znak} ${obj.Y}`)) {
-      return this.operations[obj.znak](obj.X, obj.Y);
-    } else {
-      return this.check(this.input());
-    }
-  } else {
-    return this.check(this.input());
-  }
-};
 
 console.log(math.check({ X: 5, Y: 3, znak: '-' }));
