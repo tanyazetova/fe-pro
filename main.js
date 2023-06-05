34a6ee86252fadb9c76e51ab55ff02663641007f
Tetiana Zetova <tanyazetova@gmail.com>
2023-06-05 16:08:10 +0300

    add homework-38

--- main.js
+++ main.js
@@ -1,44 +1,48 @@
-function SuperMath() {
-  this.operations = {
-    '-': (x, y) => x - y,
-    '+': (x, y) => +x + +y,
-    '/': (x, y) => x / y,
-    '*': (x, y) => x * y,
-    '%': (x, y) => (x / y) * 100,
-  };
-}
-SuperMath.prototype.check = function (obj) {
-  if (
-    obj &&
-    obj.Y &&
-    obj.X &&
-    obj.znak &&
-    Object.keys(this.operations).includes(obj.znak)
-  ) {
-    if (confirm(`Do you want to calculate ${obj.X} ${obj.znak} ${obj.Y}`)) {
-      return this.operations[obj.znak](obj.X, obj.Y);
-    } else {
-      return this.check(this.input());
-    }
-  } else {
-    return this.check(this.input());
-  }
-};
-SuperMath.prototype.input = function () {
-  const X = prompt('X = ');
-  const Y = prompt('Y = ');
-  const znak = prompt('znak = ');
-  if (!Object.keys(this.operations).includes(znak)) {
-    alert(
-      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
-        ' '
-      )}`
-    );
-    return this.input();
-  }
-  return { X, Y, znak };
-};
+class Human {
+  name;
+  gender;
 
-const math = new SuperMath();
-console.log(math.check());
-console.log(math.check({ X: 5, Y: 3, znak: '-' }));
+  constructor(name, gender) {
+    this.name = name;
+    this.gender = gender;
+  }
+}
+
+class Flat {
+  residents = [];
+  addResident(human) {
+    this.residents.push(human);
+  }
+}
+
+class House {
+  flats = [];
+  maxNumberFlats;
+
+  constructor(maxNumberFlats) {
+    this.maxNumberFlats = maxNumberFlats;
+  }
+
+  addFlat(flat) {
+    if (this.flats.length < this.maxNumberFlats) {
+      this.flats.push(flat);
+    } else {
+      console.log('the maximum number of flats has been exceeded');
+    }
+  }
+}
+
+const human1 = new Human('Valera', 'man');
+const human2 = new Human('Lena', 'woman');
+const human3 = new Human('Igor', 'man');
+const flat1 = new Flat();
+const flat2 = new Flat();
+const flat3 = new Flat();
+flat1.addResident(human1);
+flat2.addResident(human1);
+flat2.addResident(human2);
+flat3.addResident(human3);
+const house = new House(2);
+house.addFlat(flat1);
+house.addFlat(flat2);
+house.addFlat(flat3);
