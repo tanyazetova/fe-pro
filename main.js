4e14bbf31c805ff8f221ef91a1d26ca88aca8eb9
Tetiana Zetova <tanyazetova@gmail.com>
2023-06-12 11:29:45 +0300

    add homework-45

--- main.js
+++ main.js
@@ -1,44 +1,11 @@
-function SuperMath() {
-  this.operations = {
-    '-': (x, y) => x - y,
-    '+': (x, y) => +x + +y,
-    '/': (x, y) => x / y,
-    '*': (x, y) => x * y,
-    '%': (x, y) => (x / y) * 100,
-  };
-}
-SuperMath.prototype.check = function (obj) {
-  if (
-    obj &&
-    obj.Y &&
-    obj.X &&
-    obj.znak &&
-    Object.keys(this.operations).includes(obj.znak)
-  ) {
-    if (confirm(`Do you want to calculate ${obj.X} ${obj.znak} ${obj.Y}`)) {
-      return this.operations[obj.znak](obj.X, obj.Y);
-    } else {
-      return this.check(this.input());
-    }
-  } else {
-    return this.check(this.input());
-  }
-};
-SuperMath.prototype.input = function () {
-  const X = prompt('X = ');
-  const Y = prompt('Y = ');
-  const znak = prompt('znak = ');
-  if (!Object.keys(this.operations).includes(znak)) {
-    alert(
-      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
-        ' '
-      )}`
-    );
-    return this.input();
-  }
-  return { X, Y, znak };
-};
+const input1 = document.querySelector('#input1');
+const input2 = document.querySelector('#input2');
+const input3 = document.querySelector('#input3');
+const textarea = document.querySelector('#textarea');
 
-const math = new SuperMath();
-console.log(math.check());
-console.log(math.check({ X: 5, Y: 3, znak: '-' }));
+setInterval(() => {
+  const [text1, text2, text3] = textarea.value.split(',');
+  if (input1.value != text1 || input2.value != text2 || input3.value != text3) {
+    textarea.value = `${input1.value},${input2.value},${input3.value}`;
+  }
+}, 1000);
