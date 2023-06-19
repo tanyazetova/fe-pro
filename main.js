785c2d25110c5b9baf663bec652aa132f0e33969
Tetiana Zetova <tanyazetova@gmail.com>
2023-06-19 20:30:02 +0300

    fix homework code

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-const input1 = document.querySelector('#input1');
-const input2 = document.querySelector('#input2');
-const input3 = document.querySelector('#input3');
+function readInputs(inputs, textarea) {
+  const inputValues = Array.from(inputs)
+    .map((input) => input.value)
+    .join(',');
+
+  if (textarea.value !== inputValues) {
+    textarea.value = inputValues;
+  }
+}
+
+const inputs = document.querySelectorAll('.input-field');
 const textarea = document.querySelector('#textarea');
 
 setInterval(() => {
-  const [text1, text2, text3] = textarea.value.split(',');
-  if (input1.value != text1 || input2.value != text2 || input3.value != text3) {
-    textarea.value = `${input1.value},${input2.value},${input3.value}`;
-  }
+  readInputs(inputs, textarea);
 }, 1000);
