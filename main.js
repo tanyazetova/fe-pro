009e2138f99c8f447ecdb3d339b8c12a62f724d8
Tetiana Zetova <tanyazetova@gmail.com>
2023-06-28 22:52:17 +0300

    add homework 47

--- main.js
+++ main.js
@@ -1,44 +1,18 @@
-function SuperMath() {
-  this.operations = {
-    '-': (x, y) => x - y,
-    '+': (x, y) => +x + +y,
-    '/': (x, y) => x / y,
-    '*': (x, y) => x * y,
-    '%': (x, y) => (x / y) * 100,
-  };
-}
-SuperMath.prototype.check = function (obj) {
-  if (
-    obj &&
-    obj.Y &&
-    obj.X &&
-    obj.znak &&
-    Object.keys(this.operations).includes(obj.znak)
-  ) {
-    if (confirm(`Do you want to calculate ${obj.X} ${obj.znak} ${obj.Y}`)) {
-      return this.operations[obj.znak](obj.X, obj.Y);
-    } else {
-      return this.check(this.input());
-    }
-  } else {
-    return this.check(this.input());
-  }
-};
-SuperMath.prototype.input = function () {
-  const X = prompt('X = ');
-  const Y = prompt('Y = ');
-  const znak = prompt('znak = ');
-  if (!Object.keys(this.operations).includes(znak)) {
-    alert(
-      `Znak incorrect, use one of these: ${Object.keys(this.operations).join(
-        ' '
-      )}`
-    );
-    return this.input();
-  }
-  return { X, Y, znak };
-};
+function drawTable(size) {
+  const table = document.querySelector('table');
+  let count = 0;
 
-const math = new SuperMath();
-console.log(math.check());
-console.log(math.check({ X: 5, Y: 3, znak: '-' }));
+  for (let row = 1; row <= size; row++) {
+    const tr = document.createElement('tr');
+
+    for (let col = 1; col <= size; col++) {
+      count++;
+      const td = document.createElement('td');
+      td.innerText = count;
+      tr.appendChild(td);
+    }
+    table.appendChild(tr);
+  }
+}
+
+drawTable(10);
